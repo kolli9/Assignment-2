3b128dbdd0dd6ff97c3d59b2043c1e18c7bdc49f
kolli9 <78166296+kolli9@users.noreply.github.com>
2021-10-13 15:25:08 +0530

    Add files via upload

---
+++ DecimalSystem.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() 
+{
+    while(1)
+    {
+        int choice;
+        printf("Select choice \n 1.Addition \n 2.Multiplication \n ");
+        scanf("%d",&choice);
+        if(choice==1)
+        {
+            long long int a,b,sum;
+            printf("Enter the values of two decimal numbers: ");
+            scanf("%lld %lld",&a,&b);
+            sum=a+b;
+            printf("sum of %d and %d is %lld\n",a,b,sum);
+        }
+        else if(choice==2)
+        {
+            long long int a,b,product;
+            printf("Enter the values of two decimal numbers: ");
+            scanf("%lld %lld",&a,&b);
+            product=a*b;
+            printf("product of %d and %d is %lld\n",a,b,product);
+        }
+        else
+       {
+            printf("Exist\n");
+       }
+    }
+}
